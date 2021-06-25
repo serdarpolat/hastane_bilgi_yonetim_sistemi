963ae04042a680894d1ad7cc290cc6ab85d721e1
serdarpolat <serdar.plt21@gmail.com>
2021-06-25 23:26:26 +0300

    Login Sayfası Veritabanına Bağlandı

--- admin/sayfalar/login.php
+++ admin/sayfalar/login.php
@@ -1,11 +1,19 @@
+<?php
+require_once "../../system/settings.php";
+require_once "../../system/functions.php";
+
+
+?>
 <!DOCTYPE html>
 <html lang="tr">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width", initial-scale=1.0>
-    <link rel"stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width" , initial-scale=1.0>
+    <link rel="stylesheet" href="login.css">
     <title> HASTANE OTOMASYONU </title>
 </head>
+
 <body>
     <header>
         <h2> HASTANE OTOMASYONU</h2>
@@ -15,16 +23,32 @@
         <h1> Giriş Yap</h1>
         <form action="islem.php" method="post">
             <div class="user">
-                <input type="text" placeholder="Tc Kimlik No">            
+                <input type="text" name="tck" placeholder="Tc Kimlik No">
             </div>
             <div class="pass">
-                <input type="password" placeholder="Şifre">            
+                <input type="password" name="pass" placeholder="Şifre">
             </div>
             <div class="pass">
-                <button type="submit" class="sub" id="giris"> Giriş Yap</button>          
-                <br>  
+                <button type="submit" name="submit" class="sub" id="giris"> Giriş Yap</button>
+                <br>
             </div>
         </form>
-        <a href="uye.php"><button type="submit" id="uye">Üye Ol</button></a>
+        <a href="register.php"><button id="uye">Üye Ol</button></a>
+        <?php
+        if ($_POST['submit']) {
+            $tck = $_POST['tck'];
+            $pass = md5($_POST['pass']);
+
+            $query = query($conn, "SELECT * FROM users WHERE tck='$tck' AND pass='$pass'");
+
+            $res = num_rows($conn);
+
+            if ($res) {
+                go("../../site/index.php");
+            } else {
+                echo "Bilgilerinizi Kontrol Edin";
+            }
+        }
+        ?>
     </div>
-</body>    
+</body>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require_once "system/settings.php";
+require_once "system/system.php";
+
+if ($ayar['status'] == 1) {
+    require "./site/index.php";
+}
