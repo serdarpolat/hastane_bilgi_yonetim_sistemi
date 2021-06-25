e67f139ee35d352464feb81fa3f3006e72c250fd
selamikadioglu <selami-kadioglu@outlook.com>
2021-06-25 23:54:17 +0300

    anasayfa header ve header.css oluşturuldu

---
+++ admin/sayfalar/anasayfa.php
@@ -0,0 +1,16 @@
+<?php
+include 'header.php'
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width", initial-scale=1.0>
+    <link rel"stylesheet" href="header.css">
+    <title> HASTANE OTOMASYON </title>
+</head>
+<body>
+</body>
