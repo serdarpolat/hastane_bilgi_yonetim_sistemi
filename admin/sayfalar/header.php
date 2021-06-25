c4f1bf1526267a66091da3cc48f50b4990657e70
selamikadioglu <selami-kadioglu@outlook.com>
2021-06-25 23:51:23 +0300

    anasayfa header ve header.css oluşturuldu.

---
+++ admin/sayfalar/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width", initial-scale=1.0>
+    <link rel"stylesheet" href="header.css">
+    <title> HASTANE OTOMASYON </title>
+</head>
+<body>
+    <div class="ust_bar">
+        <a href="anasayfa.php"><h1> HASTANE OTOMASYONU</h1></a>
+        <div class="menu">
+        <a href="hesap.php"><h5> Hesap Bilgileri</h5></a>
+        <a href="randevu.php"><h5> Randevu Bilgileri</h5></a>
+        </div>   
+    </div>
+
+    <div class="cikis">
+        <a href="hesap.php"><h5> Hesap Bilgileri</h5></a>
+        <a href="hesap.php"><h5> Randevu Bilgileri</h5></a>
+
+        
+    </div>
+</body>
+</hmtl>
