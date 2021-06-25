8e3f593fc0e683819f63cc45e9ecb93adc0045b7
serdarpolat <serdar.plt21@gmail.com>
2021-06-25 23:40:39 +0300

    settings.php ve functions.php kodlandı, login.php veritabanına bağlandı

--- admin/sayfalar/login.php
+++ admin/sayfalar/login.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width" , initial-scale=1.0>
-    <link rel"stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css">
     <title> HASTANE OTOMASYONU </title>
 </head>
 
@@ -28,5 +28,23 @@
             </div>
         </form>
         <a href="uye.php"><button type="submit" id="uye">Üye Ol</button></a>
+        <?php
+        if (isset($_POST['submit'])) {
+            $tck = $_POST['tck'];
+            $pass = md5($_POST['pass']);
+
+            if ($tack && $pass) {
+                $query = query($conn, "select * from user where tck='$tck' and $pass='$pass'");
+
+                $row = num_rows($query);
+
+                if ($row) {
+                    go("../../site/index.php");
+                }
+
+                echo "bilgilerinizi kontrol ediniz";
+            }
+        }
+        ?>
     </div>
 </body>
