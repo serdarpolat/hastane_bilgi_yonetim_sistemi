11dcac69742fcd3e94f94821859806bd791a065e
serdarpolat <serdar.plt21@gmail.com>
2021-06-26 00:04:12 +0300

    fix

--- admin/sayfalar/anasayfa.php
+++ admin/sayfalar/anasayfa.php
@@ -1,16 +1,17 @@
-<?php
-include 'header.php'
-
-
-?>
-
 <!DOCTYPE html>
 <html lang="tr">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width", initial-scale=1.0>
-    <link rel"stylesheet" href="header.css">
+    <meta name="viewport" content="width=device-width" , initial-scale=1.0>
+    <link rel="stylesheet" href="header.css">
     <title> HASTANE OTOMASYON </title>
 </head>
+
 <body>
+    <?php
+    include 'header.php'
+    ?>
 </body>
+
+</html>
